2386a5708d86db4bcd549c792b42c1ba8bda15c0
Soujanyo Biswas <biswassoujanyo@gmail.com>
2020-05-19 22:37:44 +0530

    created express server and rendered login page

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.set('view-engine', 'ejs');
+
+app.get('/', (request, response) => {
+    response.render("login.ejs");
+});
+
+app.listen(port, () => console.log(`App running at port ${port}`));
